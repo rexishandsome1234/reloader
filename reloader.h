4a6fdf6284d3fc8272d3616e04a6669bc567324c
rexishandsome1234 <rexyeung000009@gmail.com>
2024-08-02 22:15:40 +0800

    Update reloader.h

--- reloader.h
+++ reloader.h
@@ -1,1 +1,11 @@
-
+class servo{
+  private:
+  uint8_t count,servopin,button;
+  public:
+  uint8_t angle;
+  int loop;
+  void buttonpin();
+  void getData();
+  void write();
+  int buttonstate();
+}
