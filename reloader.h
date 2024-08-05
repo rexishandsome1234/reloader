47ddf63328bf80f721dffb0db232af3cd0ddff8f
rexishandsome1234 <rexyeung000009@gmail.com>
2024-08-05 10:21:02 +0800

    Update reloader.h

--- reloader.h
+++ reloader.h
@@ -1,11 +1,17 @@
-class servo{
+#pragma once
+#include <Arduino.h>
+#include<stdint.h>
+class reloader {
   private:
-  uint8_t count,servopin,button;
+  uint8_t count,servopin,buttonpin,servo2pin,button2pin,servo2degree;
+  
   public:
   uint8_t angle;
   int loop;
-  void buttonpin();
-  void getData();
+  myservo(uint8_t servopin, uint8_t buttonpin, uint8_t count);
+  void attach(uint8_t buttonpin);
   void write();
   int buttonstate();
+  void reset();
+  void getcount();
 }
